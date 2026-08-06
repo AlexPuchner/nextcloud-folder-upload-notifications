edbaf23b93fa3f1e5e36c65cd4a1920e519c4f89
AlexPuchner <167778651+AlexPuchner@users.noreply.github.com>
2026-08-06 20:14:19 +0200

    scaffold event-driven Nextcloud app

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+require_once './vendor-bin/cs-fixer/vendor/autoload.php';
+
+use Nextcloud\CodingStandard\Config;
+
+$config = new Config();
+$config
+	->getFinder()
+	->notPath('build')
+	->notPath('vendor')
+	->notPath('vendor-bin/*/vendor')
+	->in(__DIR__);
+
+return $config;
