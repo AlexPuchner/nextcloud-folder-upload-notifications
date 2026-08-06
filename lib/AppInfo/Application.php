225a0fa11fbda4d196a1420f6f914c621d574c2c
AlexPuchner <167778651+AlexPuchner@users.noreply.github.com>
2026-08-06 20:14:38 +0200

    scaffold event-driven Nextcloud app

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 AlexPuchner
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\FolderUploadNotifications\AppInfo;
+
+use OCA\FolderUploadNotifications\Listener\NodeCreatedListener;
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\Files\Events\Node\NodeCreatedEvent;
+
+final class Application extends App implements IBootstrap {
+	public const APP_ID = 'folder_upload_notifications';
+
+	/** @psalm-suppress PossiblyUnusedMethod */
+	public function __construct() {
+		parent::__construct(self::APP_ID);
+	}
+
+	public function register(IRegistrationContext $context): void {
+		$context->registerEventListener(NodeCreatedEvent::class, NodeCreatedListener::class);
+	}
+
+	public function boot(IBootContext $context): void {
+	}
+}
