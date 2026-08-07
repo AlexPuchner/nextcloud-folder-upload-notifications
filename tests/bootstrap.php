2e3b3ad7312c57b7f2c5da996f19cffc16736498
AlexPuchner <167778651+AlexPuchner@users.noreply.github.com>
2026-08-07 02:11:53 +0200

    Test against Nextcloud runtime

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-if (!class_exists(\OCP\BackgroundJob\QueuedJob::class)) {
-	require_once __DIR__ . '/stubs/queued_job.php';
+$nextcloudRoot = getenv('NEXTCLOUD_ROOT');
+if (!is_string($nextcloudRoot) || !is_file($nextcloudRoot . '/lib/composer/autoload.php')) {
+	throw new RuntimeException('NEXTCLOUD_ROOT must point to an extracted Nextcloud server release');
 }
+
+require_once $nextcloudRoot . '/lib/composer/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
